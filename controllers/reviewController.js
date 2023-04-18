27b07d8e86f35cfaef7c0c65902b9ba034c6dd1d
Anna Lebid <annalez16@gmail.com>
2023-04-18 18:37:02 +0300

    add createReview controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,52 @@
+import StatusCodes from "http-status-codes";
+import { Review } from "../models/Review.js";
+import { Product } from "../models/Product.js";
+import { checkPermissions } from "../utils/index.js";
+import {
+  NotFoundError,
+  BadRequestError,
+  UnauthenticatedError,
+} from "../errors/index.js";
+
+export const createReview = async (req, res) => {
+  const { product: productId } = req.body;
+  const { userId } = req.user;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+
+  if (!isValidProduct) {
+    throw new NotFoundError(`No product with id: ${productId}`);
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new BadRequestError(`Already submitted review for this product`);
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
+};
+
+export const getAllReviews = async (req, res) => {
+  const reviews = await Review.find({});
+
+  res.status(StatusCodes.OK).json({ reviews });
+};
+
+export const getSingleReview = async (req, res) => {
+  res.send("getSingleReview");
+};
+
+export const updateReview = async (req, res) => {
+  res.send("updateReview");
+};
+
+export const deleteReview = async (req, res) => {
+  res.send("deleteReview");
+};
