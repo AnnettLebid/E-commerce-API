33a4d2fbb9c94ca96a6011afd71c0149e9c7849e
Anna Lebid <annalez16@gmail.com>
2023-04-19 13:29:25 +0300

    update productRoutes with getSingleProductReviews

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,6 +12,8 @@ import {
   uploadImage,
 } from "../controllers/productController.js";
 
+import { getSingleProductReviews } from "../controllers/reviewController.js";
+
 const router = express.Router();
 
 router
@@ -28,5 +30,7 @@ router
   .get(getSingleProduct)
   .patch([authenticateUser, authorizePermissions("admin")], updateProduct)
   .delete([authenticateUser, authorizePermissions("admin")], deleteProduct);
+
+router.route("/:id/reviews").get(getSingleProductReviews);
 
 export default router;
