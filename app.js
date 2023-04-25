741b06472c30c5e3ebbe3bfb32bf2c2ec554c22b
Anna Lebid <annalez16@gmail.com>
2023-04-26 01:14:23 +0300

    create orderRoutes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import reviewRouter from "./routes/reviewRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
 
 const port = process.env.PORT || 5001;
 
---
+++ routes/orderRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+  updateOrder,
+  getCurrentUserOrders,
+} from "../controllers/orderController.js";
+import {
+  authenticateUser,
+  authorizePermissions,
+} from "../middleware/authentication.js";
+
+const router = express.Router();
+router
+  .route("/")
+  .post(authenticateUser, createOrder)
+  .get(authenticateUser, authorizePermissions("admin"), getAllOrders);
+
+router.route("/showAllMyOrders").get(authenticateUser, getCurrentUserOrders);
+
+router
+  .route("/:id")
+  .get(authenticateUser, getSingleOrder)
+  .patch(authenticateUser, updateOrder)
+  .delete(authenticateUser, deleteReview);
+
+export default router;
