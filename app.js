799d9619585384e50ac171f6eaa044b2bfb4c684
Anna Lebid <annalez16@gmail.com>
2023-04-29 21:53:12 +0300

    add security packages

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import rateLimiter from "express-rate-limit";
+import helmet from "helmet";
+import xss from "xss-clean";
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
 import "express-async-errors";
 import "dotenv/config.js";
 import { connectDB } from "./db/connect.js";
@@ -14,10 +19,18 @@ import reviewRouter from "./routes/reviewRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
+
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("/public"));
 app.use(fileUpload());
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 60 }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
